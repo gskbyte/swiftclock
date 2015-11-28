7c45f986b5248aa32b60c3728ca751d5305fe89e
Jose Alcalá Correa <jose.alcala@xing.com>
2015-11-28 11:50:01 +0100

    Initial Commit

---
+++ swiftclock/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftclock
+//
+//  Created by Jose Alcalá-Correa on 28/11/15.
+//  Copyright © 2015 gskbyte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
